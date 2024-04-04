2341fb57aac466a2fbb6612569797dc52cfff6a6
Pabjack <61168830+Pabjack@users.noreply.github.com>
2024-04-03 18:10:38 -0600

    modificacion del enrutador

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,38 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/404', function () {
+    return view('404');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/courses', function () {
+    return view('courses');
+});
+
+Route::get('/team', function () {
+    return view('team');
+});
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+});
+Route::get('/profile', function () {
+    return view('profile');
 });
