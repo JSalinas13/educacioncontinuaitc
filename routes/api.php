5d89ebc7676134120d869e6874c2e812f99104f5
profitamoleRuiz <20030935@itcelaya.edu.mx>
2024-04-05 12:22:44 -0600

    Creado CRUD backend para la tabla Estado. Se cambio el nombre del controlador de Estado a EstadoController; Se añnadieron las rutas de estado en el archivo api.php y se añnadio timestamp=false en el modelo de Estadoó

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstadoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//ESTADO
+Route::get('/estado', [EstadoController::class, 'index']); //lista todos los estados
+Route::get('/estado/{id}', [EstadoController::class, 'show']); //muestra un estado
+Route::post('/estado', [EstadoController::class, 'store']); //nuevo estado
+Route::put('/estado/{id}', [EstadoController::class, 'update']); //actualiza un estado por su ID
+Route::delete('/estado/{id}', [EstadoController::class, 'destroy']); //elimina un estado por su ID
+
