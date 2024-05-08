7cb397be8580ee580eea955bb724689f89c47681
lucerosz <20030452@itcelaya.edu.mx>
2024-05-08 16:48:38 -0600

    Agreguè CRUD de localidad

---
+++ app/Http/Controllers/LocalidadController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocalidadController extends Controller
+{
+    public function index()
+    {
+        $localidades = Localidad::all();
+        return response()->json($localidades );
+    }
+    public function show($id)
+    {
+        $localidad= Localidad::find($id);
+        return response()->json($localidad);
+    }
+
+    public function store(Request $request)
+    {
+        $localidad = new Localidad();
+        $localidad->localidad = $request->input('localidad');
+        $localidad->ciudad_id = $request->input('ciudad_id');
+        $localidad->save();
+        return response()->json($localidad, 201);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $localidad = Localidad::find($id);
+        $localidad->localidad = $request->input('localidad');
+        $localidad->ciudad_id = $request->input('ciudad_id');
+        $localidad->save();
+        return response()->json($localidad);
+    }
+
+    public function destroy($id)
+    {
+        $localidad= Localidad::find($id);
+        $localidad->delete();
+        return response()->json(null, 204);
+    }
+}
