f8cba42cebca38e9836a6bedf55abf60ac488e3f
Alan Emmanuel Barcenas Castillo <20030957@itcelaya.edu.mx>
2024-05-23 02:56:49 -0600

    se realizaron 4 Cruds (Privilegio, PrivilegioRol, UsuarioRol y Rol) del departamento de usuarios (Alan, Yisus, Jacob y Jair)

--- app/Http/Controllers/PrivilegioRolController.php
+++ app/Http/Controllers/PrivilegioRolController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class PrivilegioRolController extends Controller
 {
-    //
+    function crearPrivilegioRol($id_privilegio, $id_rol) {
+        PrivilegioRol::create(['id_privilegio' => $id_privilegio, 'id_rol' => $id_rol]);
+        echo "PrivilegioRol creado exitosamente.\n";
+    }
+    
+    function leerPrivilegiosRoles() {
+        $privilegiosRoles = PrivilegioRol::all();
+        foreach ($privilegiosRoles as $pr) {
+            echo "ID Privilegio: {$pr->id_privilegio}, ID Rol: {$pr->id_rol}\n";
+        }
+    }
+    
+    function eliminarPrivilegioRol($id_privilegio, $id_rol) {
+        $pr = PrivilegioRol::where('id_privilegio', $id_privilegio)->where('id_rol', $id_rol)->first();
+        if ($pr) {
+            $pr->delete();
+            echo "PrivilegioRol eliminado exitosamente.\n";
+        } else {
+            echo "PrivilegioRol no encontrado.\n";
+        }
+    }
 }
