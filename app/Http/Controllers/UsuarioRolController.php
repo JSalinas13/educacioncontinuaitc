e97a7a93ba1d96549864151ffed4701b5201c0ea
AlanBarcenas <20030957@itcelaya.edu.mx>
2024-05-24 16:25:33 -0600

    Se agregaron librearias faltantes a los cruds de usuariosRoles(4)

--- app/Http/Controllers/UsuarioRolController.php
+++ app/Http/Controllers/UsuarioRolController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UsuarioRol;
 
-class UsuarioRol extends Controller
+class UsuarioRolController extends Controller
 {
     function crearUsuarioRol($id_usuario, $id_rol) {
         UsuarioRol::create(['id_usuario' => $id_usuario, 'id_rol' => $id_rol]);
